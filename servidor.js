a028ffbe99d5fbc992e22339bd56cc99ee0f3255
SoryQ14 <squibtanarosas64@gmail.com>
2024-03-12 09:32:11 -0600

    error404

--- servidor.js
+++ servidor.js
@@ -30,11 +30,11 @@ http.createServer(function (request, response){
     };
     
     contentType = mimeTypes[extname] || 'application/octet-stream'; 
-    
+
     fs.readFile(filePath, function(error, content){
         if(error){
             if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content){
+                fs.readFile('./Error404/404.html', function(error, content){
                     response.writeHead(200, { 'Content-Type': contentType}); 
                     response.end(content, 'utf-8'); 
                 });
